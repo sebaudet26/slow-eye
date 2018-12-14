0c654950fbf15f668469c3fc7c05c130b41fc0ba
Alexandre Sobolevski <sobolevski.a@gmail.com>
2018-12-14 15:01:18 -0500

    All teams roster sorted by full name

--- app/containers/HomePage/constants.js
+++ app/containers/HomePage/constants.js
@@ -1,3 +1,4 @@
 module.exports = {
   FETCH_PLAYERS: 'boilerplate/Home/FETCH_PLAYERS',
+  FETCH_TEAMS: 'boilerplate/Home/FETCH_TEAMS',
 };
--- app/containers/HomePage/reducer.js
+++ app/containers/HomePage/reducer.js
@@ -1,19 +1,11 @@
-/*
- * HomeReducer
- *
- * The reducer takes care of our data. Using actions, we can change our
- * application state.
- * To add a new action, add it to the switch statement in the reducer function
- *
- * Example:
- * case YOUR_ACTION_CONSTANT:
- *   return state.set('yourStateVariable', true);
- */
 import { fromJS } from 'immutable';
+import { path, sortBy } from 'ramda';
 import { FETCH_PLAYERS } from './constants';
 
 const initialState = fromJS({
-
+  sorts: {
+    players: ['fullName'],
+  },
 });
 
 function homeReducer(state = initialState, action) {
@@ -21,7 +13,7 @@ function homeReducer(state = initialState, action) {
   switch (action.type) {
     case FETCH_PLAYERS:
       // Delete prefixed '@' from the github username
-      return state.set('players', action.payload);
+      return state.set('players', sortBy(path(['person', 'fullName']))(action.payload));
     default:
       return state;
   }
