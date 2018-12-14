c6f0a73e80da77ba4c2cd88d59c5ca8e80b382ee
Sébastien Audet <seb.audet26@gmail.com>
2018-12-14 16:13:38 -0500

    small changes

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './style.scss';
 
 const Footer = () => (
   <footer>
-    <section>Made with <span role="img" aria-label="heart-emoji">❤️</span> by <a href="https://twitter.com/flexdinesh">Seb and Alex</a></section>
+    <section>Made with <span role="img" aria-label="heart-emoji">❤️</span> by <a target="_blank" href="http://chatler.io">Seb and Alex</a></section>
   </footer>
 );
 
