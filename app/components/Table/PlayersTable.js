3db913032e88432fbf33fa2995238b9e655a2ab2
Alexandre Sobolevski <sobolevski.a@gmail.com>
2018-12-14 16:28:07 -0500

    Use react-table

--- app/components/Table/PlayersTable.js
+++ app/components/Table/PlayersTable.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 import './styles.scss';
-
-const makePlayerString = player => (
-  <div key={player.id}>{player.person.fullName}</div>
-);
 
 class PlayersTable extends React.PureComponent {
   render() {
     const { players } = this.props;
     return (
       <div>
-        {players ? players.map(makePlayerString) : null}
+        <ReactTable
+          data={players}
+          columns={[
+            {
+              Header: 'Name',
+              id: 'fullName',
+              accessor: d => d.person.fullName,
+            },
+            {
+              Header: 'Goals',
+              id: 'goals',
+              accessor: d => d.stats[0].stat.goals || 0,
+              className: 'center',
+            },
+            {
+              Header: 'Assists',
+              id: 'assists',
+              accessor: d => d.stats[0].stat.assists || 0,
+              className: 'center',
+            },
+            {
+              Header: 'Points',
+              id: 'points',
+              accessor: d => d.stats[0].stat.points || 0,
+              className: 'center',
+            },
+          ]}
+          defaultPageSize={10}
+          className="-striped -highlight"
+        />
       </div>
     );
   }
