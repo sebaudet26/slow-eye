90f7cf7fabf0414a694c47e4f7a0ce2fabc1999b
Alexandre Sobolevski <sobolevski.a@gmail.com>
2018-12-14 17:01:55 -0500

    Mock players for now

--- app/containers/HomePage/reducer.js
+++ app/containers/HomePage/reducer.js
@@ -14,7 +14,6 @@ function homeReducer(state = initialState, action) {
       // Delete prefixed '@' from the github username
       return state.set('players', sortBy(path(['person', 'fullName']))(action.payload));
     default:
-      //return state.set('players', playersMock);
       return state;
   }
 }
