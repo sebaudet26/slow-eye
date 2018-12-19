cb9e184fe67148a4dc802c64eafbc89983e75b8b
Sébastien Audet <seb.audet26@gmail.com>
2018-12-18 22:02:00 -0500

    favicon yeyyy

--- app/app.js
+++ app/app.js
@@ -15,7 +15,7 @@ import App from './containers/App';
 
 // Load the favicon
 /* eslint-disable import/no-webpack-loader-syntax */
-// import '!file-loader?name=[name].[ext]!./images/favicon.ico';
+import '!file-loader?name=[name].[ext]!./images/favicon.ico';
 /* eslint-enable import/no-webpack-loader-syntax */
 
 // Import CSS reset and Global Styles
