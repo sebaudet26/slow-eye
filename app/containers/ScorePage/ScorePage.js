ddfd71374f046127ded2e05fc9eba9c3d2aed768
Sébastien Audet <seb.audet26@gmail.com>
2018-12-20 14:41:04 -0500

    Scores

--- app/containers/ScorePage/ScorePage.js
+++ app/containers/ScorePage/ScorePage.js
@@ -18,6 +18,77 @@ export default class ScorePage extends React.Component {
           />
         </Helmet>
         <h2>Scores</h2>
+        <div className="scoreboard">
+          <div className="game-card">
+            <div className="game-card-header">
+              Final
+            </div>
+            <div className="game-card-team">
+              <img src="../../images/teams/small/COL.png" />
+              <div className="game-card-team-name">
+                Colorado Avalanche
+              </div>
+              <div className="game-card-team-score">
+              <span>1</span>
+              </div>
+            </div>
+            <div className="game-card-team">
+              <img src="../../images/teams/small/MTL.png" />
+              <div className="game-card-team-name">
+                Montreal Canadiens
+              </div>
+              <div className="game-card-team-score">
+              2
+              </div>
+            </div>
+          </div>
+          <div className="game-card">
+            <div className="game-card-header">
+              Live - 20:00 2nd Period
+            </div>
+            <div className="game-card-team">
+              <img src="../../images/teams/small/MIN.png" />
+              <div className="game-card-team-name">
+                Minnesota Wild
+              </div>
+              <div className="game-card-team-score">
+              <span>7</span>
+              </div>
+            </div>
+            <div className="game-card-team">
+              <img src="../../images/teams/small/SJS.png" />
+              <div className="game-card-team-name">
+                San Jose Sharks
+              </div>
+              <div className="game-card-team-score">
+              4
+              </div>
+            </div>
+          </div>
+          <div className="game-card">
+            <div className="game-card-header">
+              8:30 ET
+            </div>
+            <div className="game-card-team">
+              <img src="../../images/teams/small/BUF.png" />
+              <div className="game-card-team-name">
+                Buffalo Sabres
+              </div>
+              <div className="game-card-team-score">
+              <span>44 Pts</span>
+              </div>
+            </div>
+            <div className="game-card-team">
+              <img src="../../images/teams/small/LAK.png" />
+              <div className="game-card-team-name">
+                Los Angeles Kings
+              </div>
+              <div className="game-card-team-score">
+                22 Pts
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
