f61cf801d8191d067d2092e6fc29e2ec950bbda0
Sébastien Audet <seb.audet26@gmail.com>
2019-01-21 16:05:27 -0500

    Score Update

---
+++ app/components/DatePagination/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import './style.scss';
+
+class DatePagination extends React.Component {
+  render() {
+    return (
+      <div>
+        Test
+      </div>
+    );
+  }
+}
+
+export default DatePagination;
