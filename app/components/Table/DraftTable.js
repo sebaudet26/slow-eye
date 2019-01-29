19f287be9ec7e5ee2c23d7861283e03630306151
Sébastien Audet <seb.audet26@gmail.com>
2019-01-28 22:41:04 -0500

    Draft Page Team Image

--- app/components/Table/DraftTable.js
+++ app/components/Table/DraftTable.js
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactTable from 'react-table';
 import {
-  map, pipe, prop, toLower, toString, length, match,
+  map, pipe, prop, toLower, toString, length, match, findLastIndex,
 } from 'ramda';
 import PlayerName from '../PlayerName';
 import TeamLogo from '../TeamLogo';
@@ -13,114 +13,117 @@ import './styles.scss';
 
 const DraftTable = ({ draft, filters, round }) => {
   console.log(filters);
-  return (
-    <ReactTable
-      data={draft}
-      resizable={false}
-      filtered={[
-        {
-          id: 'position',
-          value: filters.posSelected,
+  return (<ReactTable
+    data={draft}
+    resizable={false}
+    filtered={[
+      {
+        id: 'position',
+        value: filters.posSelected,
+      },
+      {
+        id: 'countryCode',
+        value: filters.natSelected,
+      },
+      {
+        id: 'nationality',
+        value: filters.natSelected,
+      },
+      {
+        id: 'roundNumber',
+        value: filters.roundSelected,
+      },
+    ]}
+    noDataText="Loading all dat good data stuff..."
+    columns={[
+      {
+        Header: 'Rd.',
+        id: 'roundNumber',
+        className: 'text-left',
+        accessor: prop('round'),
+        maxWidth: 40,
+        minWidth: 40,
+      },
+      {
+        Header: 'Pick',
+        id: 'overallPcikNumber',
+        className: 'text-left',
+        accessor: prop('overallNumber'),
+        maxWidth: 50,
+        minWidth: 50,
+      },
+      {
+        Header: 'Team',
+        id: 'team',
+        accessor: prop('teamPickHistory'),
+        className: 'text-left team-cell',
+        Cell: pipe(
+          prop('value'),
+          map(prop('id')),
+          map(id => <TeamLogo teamId={id} season={20172018} />),
+        ),
+        maxWidth: 150,
+        minWidth: 100,
+      },
+      {
+        Header: 'Name',
+        id: 'fullName',
+        accessor: prop('name'),
+        className: 'text-left',
+        maxWidth: 200,
+        minWidth: 150,
+        Cell: row => <PlayerName id={row.original.id} name={row.value} />,
+      },
+      {
+        Header: 'Pos.',
+        id: 'position',
+        accessor: prop('position'),
+        className: 'text-left',
+        maxWidth: 75,
+        minWidth: 50,
+        filterMethod: (filter, row) => {
+          if (filter.value === 'S') {
+            return row[filter.id] !== 'G';
+          }
+          if (filter.value === 'F') {
+            return row[filter.id] === 'C' || row[filter.id] === 'LW' || row[filter.id] === 'RW';
+          }
+          return toLowerCaseAndMatch(filter, row);
         },
-        {
-          id: 'nationality',
-          value: filters.natSelected,
-        },
-        {
-          id: 'roundNumber',
-          value: filters.roundSelected,
-        },
-      ]}
-      noDataText="Loading all dat good data stuff..."
-      columns={[
-        {
-          Header: 'Rd.',
-          id: 'roundNumber',
-          className: 'text-left',
-          accessor: prop('round'),
-          maxWidth: 40,
-          minWidth: 40,
-        },
-        {
-          Header: 'Pick',
-          id: 'overallPcikNumber',
-          className: 'text-left',
-          accessor: prop('overallNumber'),
-          maxWidth: 50,
-          minWidth: 50,
-        },
-        {
-          Header: 'Team',
-          id: 'team',
-          accessor: prop('teamPickHistory'),
-          className: 'text-left team-cell',
-          Cell: pipe(
-            prop('value'),
-            map(prop('id')),
-            map(id => <TeamLogo teamId={id} season={20172018} />),
-          ),
-          maxWidth: 150,
-          minWidth: 100,
-        },
-        {
-          Header: 'Name',
-          id: 'fullName',
-          accessor: prop('name'),
-          className: 'text-left',
-          maxWidth: 200,
-          minWidth: 150,
-          Cell: row => <PlayerName id={row.original.id} name={row.value} />,
-        },
-        {
-          Header: 'Pos.',
-          id: 'position',
-          accessor: prop('position'),
-          className: 'text-left',
-          maxWidth: 75,
-          minWidth: 50,
-          filterMethod: (filter, row) => {
-            if (filter.value === 'S') {
-              return row[filter.id] !== 'G';
-            }
-            if (filter.value === 'F') {
-              return row[filter.id] === 'C' || row[filter.id] === 'LW' || row[filter.id] === 'RW';
-            }
-            return toLowerCaseAndMatch(filter, row);
-          },
-        },
-        {
-          Header: 'Nat.',
-          id: 'nationality',
-          accessor: prop('countryCode'),
-          className: 'text-left team-cell',
-          maxWidth: 75,
-          minWidth: 50,
-          Cell: row => (
-            <img src={`/images/country/${row.value}.svg`} alt="" />
-          ),
-        },
-        {
-          Header: 'League',
-          id: 'amateurLeague',
-          accessor: prop('amateurLeague'),
-          className: 'text-left',
-          maxWidth: 150,
-          minWidth: 150,
-        },
-        {
-          Header: 'Am. Team',
-          id: 'amateurClubName',
-          accessor: prop('amateurClubName'),
-          className: 'text-left',
-          maxWidth: 150,
-          minWidth: 150,
-        },
-      ]}
-      defaultSortAsc
-      showPagination
-      className="-striped draft-table"
-      defaultPageSize={31}
-    />
+      },
+      {
+        Header: 'Nat.',
+        id: 'nationality',
+        accessor: prop('countryCode'),
+        className: 'text-left team-cell',
+        maxWidth: 75,
+        minWidth: 50,
+        Cell: row => (
+          <img src={`/images/country/${row.value}.svg`} alt="" />
+        ),
+      },
+      {
+        Header: 'League',
+        id: 'amateurLeague',
+        accessor: prop('amateurLeague'),
+        className: 'text-left',
+        maxWidth: 150,
+        minWidth: 150,
+      },
+      {
+        Header: 'Am. Team',
+        id: 'amateurClubName',
+        accessor: prop('amateurClubName'),
+        className: 'text-left',
+        maxWidth: 150,
+        minWidth: 150,
+      },
+    ]}
+    defaultSortAsc
+    showPagination
+    className="-striped draft-table"
+    defaultPageSize={31}
+  />
   );
 };
 
