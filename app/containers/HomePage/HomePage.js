84893233ae87c99ee88f29c6ba4acbc6aac0bf30
Sébastien Audet <seb.audet26@gmail.com>
2019-02-06 22:21:13 -0500

    Add hot players

--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import StandingsTable from '../../components/Table/StandingsTable';
+import FireIcon from '../../images/Fire.svg';
 import './style.scss';
 
 // const bg = require('https://nhl.bamcontent.com/images/actionshots/8478402.jpg');
@@ -20,127 +21,63 @@ export default class HomePage extends React.Component {
         </Helmet>
         <div className="home-page-wrapper">
           <div className="home-page-col">
-            <h3>3 Stars of the Week - February 4, 2019</h3>
-            <div className="home-page-col-wrapper">
-              <a href="" className="card-vertical no-bam" style={{ backgroundImage: 'url("http://images.performgroup.com/di/library/Sporting_News_CA_CMS_image_storage/2/4e/jack-roslovic-020219-getty-ftrjpeg_i1jlmevaplfp10nmkt9w80i7i.jpg?t=-1329409624")' }}>
-                <div className="card-content">
-                  <div className="card-content-number">
-                    1
-                  </div>
-                  <div className="card-content-name">
-                    Jack
-                    {' '}
-                    <span>Roslovic</span>
-                  </div>
-                </div>
-              </a>
-              <a href="" className="card-vertical no-bam" style={{ backgroundImage: 'url("http://www.philly.com/resizer/ntEQTAYV3iCwKufF5etJHPNEL3I=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/ZMEGKKXHJ5D2NGS7RSZ6JE4SNA.jpg")' }}>
-                <div className="card-content">
-                  <div className="card-content-number">
-                      2
-                  </div>
-                  <div className="card-content-name">
-                      Carter
-                    {' '}
-                    <span>Hart</span>
-                  </div>
-                </div>
-              </a>
-              <a href="" className="card-vertical" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8476459.jpg")' }}>
-                <div className="card-content">
-                  <div className="card-content-number">
-                      3
-                  </div>
-                  <div className="card-content-name">
-                      Mika
-                    {' '}
-                    <span>Zibanejad</span>
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>
-          <div className="home-page-col">
-            <h3>3 Stars of the Month - January</h3>
-            <div className="home-page-col-wrapper">
-              <a href="" className="card-vertical" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8476346.jpg")' }}>
-                <div className="card-content">
-                  <div className="card-content-number">
-                    1
-                  </div>
-                  <div className="card-content-name">
-                    Johnny
-                    {' '}
-                    <span>Gaudreau</span>
-                  </div>
-                </div>
-              </a>
-              <a href="" className="card-vertical" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8475215.jpg")' }}>
-                <div className="card-content">
-                  <div className="card-content-number">
-                    2
-                  </div>
-                  <div className="card-content-name">
-                    Robin
-                    {' '}
-                    <span>Lehner</span>
-                  </div>
-                </div>
-              </a>
-              <a href="" className="card-vertical" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8474141.jpg")' }}>
-                <div className="card-content">
-                  <div className="card-content-number">
-                    3
-                  </div>
-                  <div className="card-content-name">
-                    Patrick
-                    {' '}
-                    <span>Kane</span>
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>
-          {/*
-          <div className="home-page-col">
-            <h3>Who's hot?</h3>
-            <div className="card" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8478402_low_resolution.jpg")' }}>
+            <h3>
+              <div className="icon-wrapper">
+                <img src={FireIcon} />
+              </div>
+              {' '}
+              Who's hot?
+            </h3>
+
+            <a href="" className="card" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8478402_low_resolution.jpg")' }}>
               <div className="card-content">
+                <div className="card-content-name">
                 Connor
-                {' '}
-                <span>McDavid</span>
+                  {' '}
+                  <span>McDavid</span>
+                </div>
               </div>
-            </div>
-            <div className="card" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8474141_low_resolution.jpg")' }}>
+            </a>
+            <a href="" className="card" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8474141_low_resolution.jpg")' }}>
               <div className="card-content">
-              Patrick
-                {' '}
-                <span>Kane</span>
+                <div className="card-content-name">
+                  Patrick
+                  {' '}
+                  <span>Kane</span>
+                </div>
               </div>
-            </div>
-            <div className="card" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8478420_low_resolution.jpg")' }}>
+            </a>
+            <a href="" className="card" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8478420_low_resolution.jpg")' }}>
               <div className="card-content">
-                Mikko
-                {' '}
-                <span>Rantanen</span>
+                <div className="card-content-name">
+                  Mikko
+                  {' '}
+                  <span>Rantanen</span>
+                </div>
               </div>
-            </div>
-            <div className="card" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8476346_low_resolution.jpg")' }}>
+            </a>
+            <a href="" className="card" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8476346_low_resolution.jpg")' }}>
               <div className="card-content">
-                Johnny
-                {' '}
-                <span>Gaudreau</span>
+                <div className="card-content-name">
+                  Johnny
+                  {' '}
+                  <span>Gaudreau</span>
+                </div>
               </div>
-            </div>
-            <div className="card" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8480829_low_resolution.jpg")' }}>
+            </a>
+            <a href="" className="card" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8480829_low_resolution.jpg")' }}>
               <div className="card-content">
-                Jesperi
-                {' '}
-                <span>Kotkaniemi</span>
+                <div className="card-content-name">
+                  Jesperi
+                  {' '}
+                  <span>Kotkaniemi</span>
+                </div>
               </div>
-            </div>
+            </a>
           </div>
-          */}
+          <div className="home-page-col">
+            <h3>Power Rankings</h3>
+          </div>
         </div>
       </div>
     );
