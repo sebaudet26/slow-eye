ac0758d3efc63e35f5fee981e1e9927acd88c9ed
Sébastien Audet <seb.audet26@gmail.com>
2019-02-07 12:23:04 -0500

    Power Rankings

--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import StandingsTable from '../../components/Table/StandingsTable';
 import FireIcon from '../../images/Fire.svg';
+import PowerIcon from '../../images/boxing.svg';
 import './style.scss';
 
 // const bg = require('https://nhl.bamcontent.com/images/actionshots/8478402.jpg');
@@ -29,7 +30,7 @@ export default class HomePage extends React.Component {
               Who's hot? (Top 5)
             </h3>
 
-            <a href="" className="card" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8478402_low_resolution.jpg")' }}>
+            <a href="" className="card card-player" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8478402_low_resolution.jpg")' }}>
               <div className="card-content">
                 <div className="card-content-name">
                 Connor
@@ -47,7 +48,7 @@ export default class HomePage extends React.Component {
                 </div>
               </div>
             </a>
-            <a href="" className="card" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8474141_low_resolution.jpg")' }}>
+            <a href="" className="card card-player" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8474141_low_resolution.jpg")' }}>
               <div className="card-content">
                 <div className="card-content-name">
                   Patrick
@@ -65,7 +66,7 @@ export default class HomePage extends React.Component {
                 </div>
               </div>
             </a>
-            <a href="" className="card" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8478420_low_resolution.jpg")' }}>
+            <a href="" className="card card-player" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8478420_low_resolution.jpg")' }}>
               <div className="card-content">
                 <div className="card-content-name">
                   Mikko
@@ -83,7 +84,7 @@ export default class HomePage extends React.Component {
                 </div>
               </div>
             </a>
-            <a href="" className="card" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8476346_low_resolution.jpg")' }}>
+            <a href="" className="card card-player" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8476346_low_resolution.jpg")' }}>
               <div className="card-content">
                 <div className="card-content-name">
                   Johnny
@@ -101,7 +102,7 @@ export default class HomePage extends React.Component {
                 </div>
               </div>
             </a>
-            <a href="" className="card" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8480829_low_resolution.jpg")' }}>
+            <a href="" className="card card-player" style={{ backgroundImage: 'url("https://nhl.bamcontent.com/images/actionshots/8480829_low_resolution.jpg")' }}>
               <div className="card-content">
                 <div className="card-content-name">
                   Jesperi
@@ -121,7 +122,59 @@ export default class HomePage extends React.Component {
             </a>
           </div>
           <div className="home-page-col">
-            <h3>Power Rankings</h3>
+            <h3>
+              <div className="icon-wrapper">
+                <img src={PowerIcon} />
+              </div>
+              {' '}
+              Power Rankings
+            </h3>
+            <a href="" className="card card-team">
+              <div className="card-content">
+                <div className="card-content-rank">
+                  01
+                </div>
+                <div className="card-content-team">
+                  <svg key={Math.random()}>
+                    <use xlinkHref="/images/teams/season/20182019.svg#team-4-20182019-light" />
+                  </svg>
+                  Philadelphia
+                  {' '}
+                  <span>Flyers</span>
+                </div>
+                <div className="card-content-result">
+                  <div className="card-content-result-item">
+                    <div>Last 15 games</div>
+                    <div>
+                      12-1-2
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+            <a href="" className="card card-team">
+              <div className="card-content">
+                <div className="card-content-rank">
+                  02
+                </div>
+                <div className="card-content-team">
+                  <svg key={Math.random()}>
+                    <use xlinkHref="/images/teams/season/20182019.svg#team-7-20182019-light" />
+                  </svg>
+                  Buffalo
+                  {' '}
+                  <span>Sabres</span>
+                </div>
+                <div className="card-content-result">
+                  <div className="card-content-result-item">
+                    <div>Last 15 games</div>
+                    <div>
+                      12-1-2
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
           </div>
         </div>
       </div>
