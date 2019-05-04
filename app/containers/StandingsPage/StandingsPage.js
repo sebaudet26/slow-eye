aaced95a2832c0708f7201f4fbbfce4fbd90ef96
Sébastien Audet <seb.audet26@gmail.com>
2019-05-04 14:27:27 -0400

    Add Page Header to Standings Page

--- app/containers/StandingsPage/StandingsPage.js
+++ app/containers/StandingsPage/StandingsPage.js
@@ -14,16 +14,23 @@ export default class StandingsPage extends React.Component {
   render() {
     const { standings } = this.props;
     return (
-      <div className="standings-page container">
-        <Helmet>
-          <title>NHL Standings - SealStats.com</title>
-          <meta
-            name="description"
-            content="View NHL Standings. Seal Stats is the best place to view NHL stats. User-friendly and fast."
-          />
-        </Helmet>
-        <h2>Standings</h2>
-        <StandingsTable standings={standings} />
+      <div className="standings-page">
+        <div className="page-header">
+          <div className="container">
+            <h2>Standings</h2>
+          </div>
+        </div>
+        <div className="container">
+          <Helmet>
+            <title>NHL Standings - SealStats.com</title>
+            <meta
+              name="description"
+              content="View NHL Standings. Seal Stats is the best place to view NHL stats. User-friendly and fast."
+            />
+          </Helmet>
+
+          <StandingsTable standings={standings} />
+        </div>
       </div>
     );
   }
