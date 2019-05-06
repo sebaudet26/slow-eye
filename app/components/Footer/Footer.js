a2462900539b6000bd9b1d3bce500d5bc0b5f247
Sébastien Audet <seb.audet26@gmail.com>
2019-05-06 15:37:50 -0400

    Footer Text Align Center

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.scss';
-import PatreonLogo from './images/patreon.svg';
 
 const Footer = () => (
   <footer className="footer">
