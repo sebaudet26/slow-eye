a92a55d46f3de7eb0817477821b8eab98dc3babe
Sébastien Audet <seb.audet26@gmail.com>
2019-05-09 15:17:45 -0400

    Set up MLB Standings Page

--- app/containers/mlb/StandingsPage/index.js
+++ app/containers/mlb/StandingsPage/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './style.scss';
 import { Helmet } from 'react-helmet';
+import { fetchStandings } from '../../../../server/libs/mlbApi.js';
 
 export default class MLBStandingsPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      standings: [],
+    };
+  }
+
+  async getStandings() {
+    const standings = await fetchStandings();
+    this.setState({
+      standings,
+    });
+  }
+
+  componentDidMount() {
+    this.getStandings();
+  }
+
   render() {
     return (
-      <div className="contributor-page container">
+      <div className="standings-page">
         <Helmet>
           <title>Test</title>
           <meta
@@ -13,7 +32,14 @@ export default class MLBStandingsPage extends React.Component {
             content="Thank you to all Patreon Contributors"
           />
         </Helmet>
-        <h2>Ok got this</h2>
+        <div className="page-header">
+          <div className="container">
+            <h2>Standings</h2>
+          </div>
+        </div>
+        <div className="container">
+          {JSON.stringify(this.state.standings)}
+        </div>
       </div>
     );
   }
