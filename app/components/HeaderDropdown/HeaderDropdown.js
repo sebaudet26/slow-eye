1f17cdf036e7aac5fc8457618f5cf69430d6ef37
Sébastien Audet <seb.audet26@gmail.com>
2019-07-18 16:04:05 -0500

    Fix Toggle Error

--- app/components/HeaderDropdown/HeaderDropdown.js
+++ app/components/HeaderDropdown/HeaderDropdown.js
@@ -32,7 +32,7 @@ class HeaderDropdown extends React.Component {
     const subMenu = this.props.list;
 
     return (
-      <div className="header-nav-item headerDropdownMenu" onMouseOver={this.onMouseEnter} onMouseLeave={this.onMouseLeave} toggle={this.toggle}>
+      <div className="header-nav-item headerDropdownMenu" onMouseOver={this.onMouseEnter} onMouseLeave={this.onMouseLeave} onToggle={this.toggle}>
         <div className="">
           {this.props.name}
           <i className="headerDropdownMenu-arrow" />
