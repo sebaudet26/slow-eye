e8110758ae67135b572c6c137bf4017666377db8
Sébastien Audet <seb.audet26@gmail.com>
2019-07-18 14:25:59 -0500

    Change Loading Indicator

--- app/components/LoadingIndicator/index.js
+++ app/components/LoadingIndicator/index.js
@@ -1,1 +1,10 @@
-export { default } from './LoadingIndicator';
+import React from 'react';
+import './style.scss';
+
+const LoadingIndicator = () => (
+  <div className="loading">
+    <div className="loading__indicator" />
+  </div>
+);
+
+export default LoadingIndicator;
