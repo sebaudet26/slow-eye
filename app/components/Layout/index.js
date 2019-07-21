09ea3c0cc4986e86b2f3faf5121877e5024de2e5
Sébastien Audet <seb.audet26@gmail.com>
2019-07-21 14:54:40 -0500

    Create Layout Component

---
+++ app/components/Layout/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../Header';
+import Footer from '../Footer';
+
+
+class Layout extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        {
+          this.props.children
+        }
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Layout;
