f019c806238ff30bb1a5f6062d2443fd8a498fee
sobo <sobolevski.a@gmail.com>
2019-08-08 19:15:55 -0400

    Remove useless front end utils

--- app/utils/misc.js
+++ app/utils/misc.js
@@ -4,9 +4,4 @@ export const toOrdinal = (n) => {
   return n + (s[(v - 20) % 10] || s[v] || s[0]);
 };
 
-export const timeOnIceToSeconds = timeOneIceString => (
-  Number(timeOneIceString.split(':')[0]) * 60 +
-    Number(timeOneIceString.split(':')[1])
-);
-
 export default null;
