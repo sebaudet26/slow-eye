1fc17328379cff213150e1deea83a331dd7943c7
Sébastien Audet <seb.audet26@gmail.com>
2019-11-17 12:27:58 -0500

    sort teams alphabetically player stats

--- app/utils/sort.js
+++ app/utils/sort.js
@@ -19,4 +19,16 @@ export const sortTimeOnIce = (a, b) => {
   return Number(aTime.hours) > Number(bTime.hours) ? 1 : -1;
 };
 
+export const dynamicSort = (property) => {
+  let sortOrder = 1;
+  if (property[0] === '-') {
+    sortOrder = -1;
+    property = property.substr(1);
+  }
+  return function (a, b) {
+    const result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+    return result * sortOrder;
+  };
+};
+
 export default null;
