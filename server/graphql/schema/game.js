0f8303bb8d5bb63df3ba575e3e49538b9b453f62
Alexandre Sobolevski <alexandre.sobolevski@shopify.com>
2019-11-23 13:05:35 -0500

    Game page done

--- server/graphql/schema/game.js
+++ server/graphql/schema/game.js
@@ -40,8 +40,8 @@ const Highlight = new GraphQLObjectType({
 	},
 })
 
-const BoxscoreTeamStats = new GraphQLObjectType({
-	name: 'BoxscoreTeamStats',
+const GameTeamStats = new GraphQLObjectType({
+	name: 'GameTeamStats',
 	fields: {
 		goals: { type: GraphQLInt, resolve: resolveProp('goals') },
 	  pim: { type: GraphQLInt, resolve: resolveProp('pim') },
@@ -67,9 +67,29 @@ const GamePlayer = new GraphQLObjectType({
 	name: 'BoxscorePlayerName',
 	fields: {
 		id: { type: GraphQLInt, resolve: resolvePath(['person', 'id']) },
-		name: { type: GraphQLInt, resolve: resolvePath(['person', 'id']) },
+		name: { type: GraphQLString, resolve: resolvePath(['person', 'fullName']) },
 		jerseyNumber: { type: GraphQLInt, resolve: resolveProp('jerseyNumber') },
-		position: { type: GraphQLInt, resolve: resolvePath(['position', 'code']) },
+		position: { type: GraphQLString, resolve: resolvePath(['position', 'code']) },
+		assists: { type: GraphQLInt, resolve: resolvePath(['stats', 'skaterStats', 'assists']) },
+	  goals: { type: GraphQLInt, resolve: resolvePath(['stats', 'skaterStats', 'goals']) },
+	  shots: { type: GraphQLInt, resolve: resolvePath(['stats', 'skaterStats', 'shots']) },
+	  hits: { type: GraphQLInt, resolve: resolvePath(['stats', 'skaterStats', 'hits']) },
+	  powerPlayGoals: { type: GraphQLInt, resolve: resolvePath(['stats', 'skaterStats', 'powerPlayGoals']) },
+	  powerPlayAssists: { type: GraphQLInt, resolve: resolvePath(['stats', 'skaterStats', 'powerPlayAssists']) },
+	  penaltyMinutes: { type: GraphQLInt, resolve: resolvePath(['stats', 'skaterStats', 'penaltyMinutes']) },
+	  faceOffWins: { type: GraphQLInt, resolve: resolvePath(['stats', 'skaterStats', 'faceOffWins']) },
+	  faceoffTaken: { type: GraphQLInt, resolve: resolvePath(['stats', 'skaterStats', 'faceoffTaken']) },
+	  takeaways: { type: GraphQLInt, resolve: resolvePath(['stats', 'skaterStats', 'takeaways']) },
+	  giveaways: { type: GraphQLInt, resolve: resolvePath(['stats', 'skaterStats', 'giveaways']) },
+	  shortHandedGoals: { type: GraphQLInt, resolve: resolvePath(['stats', 'skaterStats', 'shortHandedGoals']) },
+	  shortHandedAssists: { type: GraphQLInt, resolve: resolvePath(['stats', 'skaterStats', 'shortHandedAssists']) },
+	  blocked: { type: GraphQLInt, resolve: resolvePath(['stats', 'skaterStats', 'blocked']) },
+	  plusMinus: { type: GraphQLInt, resolve: resolvePath(['stats', 'skaterStats', 'plusMinus']) },
+	  faceOffPct: { type: GraphQLFloat, resolve: resolvePath(['stats', 'skaterStats', 'faceOffPct']) },
+		timeOnIce: { type: GraphQLString, resolve: resolvePath(['stats', 'skaterStats', 'timeOnIce']) },
+	  evenTimeOnIce: { type: GraphQLString, resolve: resolvePath(['stats', 'skaterStats', 'evenTimeOnIce']) },
+	  powerPlayTimeOnIce: { type: GraphQLString, resolve: resolvePath(['stats', 'skaterStats', 'powerPlayTimeOnIce']) },
+	  shortHandedTimeOnIce: { type: GraphQLString, resolve: resolvePath(['stats', 'skaterStats', 'shortHandedTimeOnIce']) },
 	}
 })
 
@@ -78,29 +98,34 @@ const GameTeam = new GraphQLObjectType({
 	fields: {
 		teamId: { type:GraphQLInt, resolve: resolvePath(['team', 'id']) },
 		teamName: { type: GraphQLString, resolve: resolvePath(['team', 'name']) },
-    teamStats: { type: BoxscoreTeamStats, resolve: resolvePath(['teamStats', 'teamSkaterStats']) },
     coach: { type: GraphQLString, resolve: resolvePath(['coaches', 0, 'person', 'fullName']) },
-
-    // players: {
-    //   ID8477493: [Object]
-    // },
-    // goalies: [ 8478470 ],
-    // skaters: [
-    //   8477963, 8475796, 8478569
-    // ],
-    // scratches: [ 8477963, 8475796, 8478569 ],
+    teamStats: { type: GameTeamStats, resolve: resolvePath(['teamStats', 'teamSkaterStats']) },
+    playerStats: {
+    	type: new GraphQLList(GamePlayer),
+    	resolve: (doc) => Object.values(doc.players || {}),
+    },
 	}
 })
 
 const Game = new GraphQLObjectType({
 	name: 'Game',
 	fields: {
-		recap: { type: GraphQLString, resolve: doc => gameHighlightsLoader.load(doc.id).then(resolveProp('recap')) },
-		goalsHighlights: { type: new GraphQLList(Highlight), resolve: doc => gameHighlightsLoader.load(doc.id).then(resolveProp('goalsHighlights')) },
-		awayTeam: { type: GameTeam, resolve: doc => gameBoxscoreLoader.load(doc.id).then((stuff) => {
-			console.log(stuff.away.players)
-			return stuff.away
-		})}
+		recap: {
+			type: GraphQLString,
+			resolve: doc => gameHighlightsLoader.load(doc.id).then(resolveProp('recap'))
+		},
+		goalsHighlights: {
+			type: new GraphQLList(Highlight),
+			resolve: doc => gameHighlightsLoader.load(doc.id).then(resolveProp('goalsHighlights'))
+		},
+		awayTeam: {
+			type: GameTeam,
+			resolve: doc => gameBoxscoreLoader.load(doc.id).then(resolveProp('away')),
+		},
+		homeTeam: {
+			type: GameTeam,
+			resolve: doc => gameBoxscoreLoader.load(doc.id).then(resolveProp('home')),
+		}
 	},
 })
 
